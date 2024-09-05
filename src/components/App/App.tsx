3d0bca711fc3cf65e4ca7792b20e5c5e973af1d0
jesusrojaslapzo <jesus.rojas@lapzo.com>
2024-09-05 19:54:25 +0200

    fix: change framework for speedinsights

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import '@/styles/globa.css'
 import { Provider, createStore } from 'jotai';
 const myStore = createStore();
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
 
