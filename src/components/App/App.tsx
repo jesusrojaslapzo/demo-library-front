9a93739e2d2cc878e5189222933e6fd50e65f0aa
jesusrojaslapzo <jesus.rojas@lapzo.com>
2024-09-05 19:48:51 +0200

    change: adding vercel analitycs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import Main from '../pages/Main'
 import '@/styles/globa.css'
 import { Provider, createStore } from 'jotai';
 const myStore = createStore();
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
   return (
     <Provider store={myStore}>
       <Main />
+      <Analytics />
     </Provider>
   )
 }
